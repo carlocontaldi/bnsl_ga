b6b1040fe02651fe1abe0237f6fa89170874da5b
Carlo Contaldi <contaldicarlo@gmail.com>
2018-07-31 02:23:00 +0200

    Update update_mp.m

--- elite_structure_learner/update_mp.m
+++ elite_structure_learner/update_mp.m
@@ -1,7 +1,7 @@
 function [MP] = update_mp(MP,MPmax,es)
 % Update the MP thresholds in an elite-based, knowledge-driven approach.
 n = size(MP,2);
-esp = cell(1,n); % #distinct parent nodes of each vertex over all elite individuals
+esp = cell(1,n);    % #distinct parent nodes of each vertex over all elite individuals
 for e = 1:size(es,2)
     for i = 1:n
         p = find(es{e}(:,i))';
